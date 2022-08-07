e6da4e81da6766470eed4263c91bf47475de4495
renankrz <renankrz@gmail.com>
2022-08-07 20:04:49 -0300

    feat: add route to create a new category

--- server/src/api/categories.js
+++ server/src/api/categories.js
@@ -1,19 +1,35 @@
 const { aql } = require('arangojs');
 const { Router } = require('express');
 const { connection } = require('../conn');
-const { COLL_NOTES } = require('../const');
+const { COLL_CATEGORIES } = require('../const');
 const { withErrorHandling } = require('../utils');
 
 const router = Router();
 
-const readAllCategories = async (db) => withErrorHandling(async () => {
-  const notesColl = db.collection(COLL_NOTES);
+const createCategory = async (db, category) => withErrorHandling(async () => {
+  const categoriesColl = db.collection(COLL_CATEGORIES);
   const cursor = await db.query(aql`
-    LET categories = (
-      FOR n IN ${notesColl}
-        RETURN DISTINCT n.category
-    )
-    FOR c IN categories
+    INSERT ${category} INTO ${categoriesColl}
+    RETURN NEW
+  `);
+  const result = await cursor.all();
+  return result;
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const db = connection.database(process.env.DB_NAME);
+    const dbCategory = await createCategory(db, req.body);
+    res.json(dbCategory);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const readAllCategories = async (db) => withErrorHandling(async () => {
+  const categoriesColl = db.collection(COLL_CATEGORIES);
+  const cursor = await db.query(aql`
+    FOR c IN ${categoriesColl}
       SORT c
       RETURN c
   `);
--- server/src/const.js
+++ server/src/const.js
@@ -1,5 +1,7 @@
+const COLL_CATEGORIES = 'categories';
 const COLL_NOTES = 'notes';
 
 module.exports = {
+  COLL_CATEGORIES,
   COLL_NOTES,
 };
