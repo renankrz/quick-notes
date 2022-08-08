9b1d1031af42324733acf1b745758b48dcef0d9f
renankrz <renankrz@gmail.com>
2022-08-07 21:27:14 -0300

    feat: transform categories into nodes in a graph

--- server/src/api/categories.js
+++ server/src/api/categories.js
@@ -1,25 +1,30 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
 const { aql } = require('arangojs');
 const { Router } = require('express');
 const { connection } = require('../conn');
-const { COLL_CATEGORIES } = require('../const');
+const { COLL_CATEGORIES, COLL_HAS_SUBCATEGORY } = require('../const');
 const { withErrorHandling } = require('../utils');
 
 const router = Router();
 
-const createCategory = async (db, category) => withErrorHandling(async () => {
+const createCategory = async (db, category, parentId) => withErrorHandling(async () => {
   const categoriesColl = db.collection(COLL_CATEGORIES);
-  const cursor = await db.query(aql`
-    INSERT ${category} INTO ${categoriesColl}
-    RETURN NEW
-  `);
-  const result = await cursor.all();
-  return result;
+  const hasSubcategory = db.collection(COLL_HAS_SUBCATEGORY);
+  const dbCategory = await categoriesColl.save(category, {
+    returnNew: true,
+  });
+  await hasSubcategory.save({
+    _from: parentId,
+    _to: dbCategory.new._id,
+  });
+  return dbCategory.new;
 });
 
 router.post('/', async (req, res, next) => {
   try {
     const db = connection.database(process.env.DB_NAME);
-    const dbCategory = await createCategory(db, req.body);
+    const dbCategory = await createCategory(db, req.body.category, req.body.parentId);
     res.json(dbCategory);
   } catch (error) {
     next(error);
@@ -42,6 +47,24 @@ router.get('/', async (req, res, next) => {
     const db = connection.database(process.env.DB_NAME);
     const dbCategories = await readAllCategories(db);
     res.json(dbCategories);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const deleteCategory = async (db, key) => withErrorHandling(async () => {
+  const categoriesColl = db.collection(COLL_CATEGORIES);
+  const dbCategory = await categoriesColl.remove(key, {
+    returnOld: true,
+  });
+  return dbCategory.old;
+});
+
+router.delete('/:key', async (req, res, next) => {
+  try {
+    const db = connection.database(process.env.DB_NAME);
+    const deletedCategory = await deleteCategory(db, req.params.key);
+    res.json(deletedCategory);
   } catch (error) {
     next(error);
   }
--- server/src/const.js
+++ server/src/const.js
@@ -1,7 +1,9 @@
 const COLL_CATEGORIES = 'categories';
 const COLL_NOTES = 'notes';
+const COLL_HAS_SUBCATEGORY = 'hasSubcategory';
 
 module.exports = {
   COLL_CATEGORIES,
   COLL_NOTES,
+  COLL_HAS_SUBCATEGORY,
 };
