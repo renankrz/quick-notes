8b0f79bd7fef552e25434d637179df1353f3e62b
renankrz <renankrz@gmail.com>
2022-08-08 22:40:52 -0300

    feat: use mui treeview to show categories

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,14 +3,14 @@ import Categories from './Categories';
 import NotesAll from './NotesAll';
 import NoteOne from './NoteOne';
 
-function App() {
-  const containerStyle = {
-    'width': '70%',
-    'margin': 'auto',
-    'display': 'flex',
-    'justifyContent': 'space-between',
-  };
+const containerStyle = {
+  'width': '70%',
+  'margin': 'auto',
+  'display': 'flex',
+  'justifyContent': 'space-between',
+};
 
+function App() {
   return (
     <div style={containerStyle}>
       <div>
--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import * as React from 'react';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import TreeItem from '@mui/lab/TreeItem';
+import TreeView from '@mui/lab/TreeView';
 import { useQuery } from '@tanstack/react-query';
 import { readCategoriesRich } from '../api/categories';
 
 const renderTree = (node) => (
-  <ul key={node._id}>
-    <li key={node._id}>
-      {node.name}
-      {node.children.length > 0
-        ? node.children.map((c) => renderTree(c))
-        : null}
-    </li>
-  </ul>
+  <TreeItem key={node._key} nodeId={node._key} label={node.name}>
+    {node.children.length > 0
+      ? node.children.map((c) => renderTree(c))
+      : null}
+  </TreeItem>
 );
 
 function Categories() {
@@ -21,7 +22,14 @@ function Categories() {
   ) : (
     <div>
       <h1 style={{ 'textAlign': 'center' }}>Categories</h1>
-      {renderTree(query.data)}
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpanded={['root']}
+        defaultExpandIcon={<ChevronRightIcon />}
+        sx={{ height: 310, flexGrow: 1 }}
+      >
+        {renderTree(query.data)}
+      </TreeView>
     </div>
   );
 }
